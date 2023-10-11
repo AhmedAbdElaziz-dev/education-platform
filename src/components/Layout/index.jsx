1cc7325a467fa1d2f1658a67b6fe3ce086c7ef37
AhmedAbdElaziz-dev <a.abdelaziz.hamed@gmail.com>
2023-10-11 12:53:17 +0200

    Add: sidebar + layout components

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "../Sidebar";
+
+function Layout() {
+  return (
+    <>
+      <div className="w-screen h-screen p-0 m-0">
+        <Sidebar />
+      </div>
+    </>
+  );
+}
+
+export default Layout;
---
+++ src/components/Sidebar/constants.js
@@ -0,0 +1,33 @@
+import {
+  HomeIcon,
+  MagnifyingGlassIcon,
+  PlayCircleIcon,
+  BookmarkIcon,
+  UserIcon,
+} from "@heroicons/react/20/solid";
+export const sidebarItems = [
+  {
+    icon: HomeIcon,
+    label: "home",
+  },
+  {
+    icon: MagnifyingGlassIcon,
+    label: "search",
+  },
+  {
+    icon: PlayCircleIcon,
+    label: "courses",
+  },
+  {
+    icon: HomeIcon,
+    label: "home",
+  },
+  {
+    icon: BookmarkIcon,
+    label: "saved",
+  },
+  {
+    icon: UserIcon,
+    label: "profiles",
+  },
+];
