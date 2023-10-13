0b0ac6a706333d4dc7d030d73a7e74e70ccf380b
AhmedAbdElaziz-dev <a.abdelaziz.hamed@gmail.com>
2023-10-13 10:12:15 +0200

    Add: course details default props

--- src/components/CourseDetails/index.jsx
+++ src/components/CourseDetails/index.jsx
@@ -1,6 +1,13 @@
+/* eslint-disable react/prop-types */
 import { ClockIcon, StarIcon } from "@heroicons/react/20/solid";
 
-function CourseDetails() {
+function CourseDetails({
+  title = "Recomended Courses",
+  duration = "45m",
+  rate = "3.5/5.0",
+  discription = `This online course was created for you if you are interestd in
+  mastering digital markting and learning how to promote your business`,
+}) {
   return (
     <div className="flex flex-col justify-between bg-zinc-100 p-10">
       <div className="h-1/2 mb-5 ">
@@ -10,22 +17,19 @@ function CourseDetails() {
         />
       </div>
       <div>
-        <h1 className="text-xl mb-5 font-medium">Recomended Courses</h1>
+        <h1 className="text-xl mb-5 font-medium">{title}</h1>
         <div className="flex items-center text-sm">
           <ClockIcon className="h-4 w-4 mr-1" />
-          <label> 45m</label>
+          <label>{duration} </label>
         </div>
         <div className="flex items-center text-sm">
           <StarIcon className="h-4 w-4 mr-1" />
-          <label> 3.5/5.0</label>
+          <label> {rate} 3.5/5.0</label>
         </div>
       </div>
       <div className="mt-5">
         <h1 className="text-xl mb-2 font-medium">Course Discription</h1>
-        <p className="text-sm">
-          This online course was created for you if you are interestd in
-          mastering digital markting and learning how to promote your business{" "}
-        </p>
+        <p className="text-sm">{discription}</p>
       </div>
       <div className="flex justify-around mt-5">
         <button className="w-1/3 border p-2 rounded-md bg-white border-black">
